fd5e6892e86ae76755404ecb0ceeca93130dc7f2
zhaol <170736012@qq.com>
2021-06-21 09:05:37 +0800

    feat:初始提交

---
+++ src/main/java/com/example/tiandilixin/TiandilixinApplication.java
@@ -0,0 +1,13 @@
+package com.example.tiandilixin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiandilixinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TiandilixinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tiandilixin/TiandilixinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tiandilixin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiandilixinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
